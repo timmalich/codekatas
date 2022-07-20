0b79a6ada6f1faf46e9610a0773820e50fb6f61b
Tim Malich <tim.malich@gmail.com>
2022-07-20 14:49:40 +0200

    progress

--- csv-viewer/kata-2022-07-20/src/main/java/de/tmalich/csvviewer/CsvViewer.java
+++ csv-viewer/kata-2022-07-20/src/main/java/de/tmalich/csvviewer/CsvViewer.java
@@ -1,7 +1,62 @@
 package de.tmalich.csvviewer;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CsvViewer {
-    public static void main(String args[]) {
-        System.out.println("FOOOOOOO" + args);
+
+    List<String> lines;
+    String csvPath;
+
+    static final String CELL_BORDER = "|";
+
+    static final String SEPERATOR = ";";
+
+    static final String MINUS = "-";
+
+    public static void main(String args[]) throws IOException {
+        CsvViewer viewer = new CsvViewer(args);
+        viewer.load();
+    }
+
+    public CsvViewer(String[] args) {
+        if (args == null || args.length < 1) {
+            throw new IllegalArgumentException();
+        }
+        csvPath = args[0];
+    }
+
+    public void load() throws IOException {
+        lines = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(csvPath))) {
+            String line = reader.readLine();
+            while (line != null) {
+                lines.add(line);
+                line = reader.readLine();
+            }
+        }
+    }
+
+    protected List<String> getFormattedLines() {
+        String line = lines.get(0);
+        String[] columns = line.split(SEPERATOR);
+        List<String> formattedLines = new ArrayList<>();
+        StringBuilder header = new StringBuilder();
+        StringBuilder headerSeperator = new StringBuilder();
+        for (String column : columns) {
+            header.append(CELL_BORDER);
+            header.append(column);
+            headerSeperator.append(CELL_BORDER);
+            headerSeperator.append(MINUS.repeat(column.length()));
+        }
+        header.append(CELL_BORDER);
+        headerSeperator.append(CELL_BORDER);
+        formattedLines.add(header.toString());
+        formattedLines.add(headerSeperator.toString());
+
+        return formattedLines;
     }
 }
--- csv-viewer/kata-2022-07-20/src/test/java/de/tmalich/csvviewer/CsvViewerTests.java
+++ csv-viewer/kata-2022-07-20/src/test/java/de/tmalich/csvviewer/CsvViewerTests.java
@@ -1,12 +1,66 @@
 package de.tmalich.csvviewer;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CsvViewerTests {
 
-	@Test
-	void mainIsNullSafe() {
-		CsvViewer.main(null);
-	}
+    CsvViewer csvViewer;
 
+    @BeforeEach
+    void setup() {
+        csvViewer = new CsvViewer(new String[]{"./src/test/resources/csv.csv"});
+    }
+
+    @Test
+    void constructorWithNullArgument_throwsIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class,
+                () -> new CsvViewer(null));
+    }
+
+    @Test
+    void constructorWithEmptyArray_throwsIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class,
+                () -> new CsvViewer(new String[0]));
+    }
+
+    @Test
+    public void load_loadsValidInputStream() throws Exception {
+        csvViewer.load();
+        assertNotNull(csvViewer.lines);
+        assertEquals(8, csvViewer.lines.size());
+    }
+
+
+    @Test
+    public void inputList_returnSingleHeader() {
+		// expexcted
+		// |h1|
+		// |--|
+        csvViewer.lines = List.of("h1");
+
+        assertEquals(Arrays.asList("|h1|", "|--|"), csvViewer.getFormattedLines());
+    }
+
+    @Test
+    public void inputList_returnHeaderWithTwoColumns() {
+		// expexcted
+		// |h1|h2|
+		// |--|--|
+        csvViewer.lines = List.of("h1;h2");
+
+        assertEquals(List.of("|h1|h2|", "|--|--|"), csvViewer.getFormattedLines());
+    }
+
+
+//    @Test
+//    public void inputList_isReturnedFormatted() {
+//        csvViewer.lines = Arrays.asList("h1;h2", "v-1;v-2");
+//        // assertEquals(Arrays.asList("h1 |h2 ", "v-1|v-2"), csvViewer.getFormattedLines());
+//    }
 }
