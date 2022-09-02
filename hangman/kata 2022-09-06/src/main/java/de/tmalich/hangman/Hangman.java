75c48a13e0b68a7ae12989b68154d919a8ed7a9e
Tim Malich <tim.malich@gmail.com>
2022-09-02 17:12:20 +0200

    Prepared hangman kata

---
+++ hangman/kata 2022-09-06/src/main/java/de/tmalich/hangman/Hangman.java
@@ -0,0 +1,7 @@
+package de.tmalich.hangman;
+
+public class Hangman {
+    public static void main(String args[]) {
+        System.out.println("FOOOOOOO" + args);
+    }
+}
---
+++ hangman/kata 2022-09-06/src/test/java/de/tmalich/hangman/HangmanTests.java
@@ -0,0 +1,12 @@
+package de.tmalich.hangman;
+
+import org.junit.jupiter.api.Test;
+
+class HangmanTests {
+
+	@Test
+	void main_isExecutable() {
+		Hangman.main(null);
+	}
+
+}
